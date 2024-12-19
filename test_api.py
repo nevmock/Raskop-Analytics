88e0d8da921fbc32901227ee06fcaf6fa0c1524e
Putu Padmanaba <157343566+Padmanaba231@users.noreply.github.com>
2024-12-19 18:20:19 +0700

    Create test_api.py

---
+++ test_api.py
@@ -0,0 +1,14 @@
+import requests
+
+# Tentukan start_date dan end_date
+params = {
+    "start_date": "2024-01-01",  # Ganti dengan tanggal awal
+    "end_date": "2024-01-31"     # Ganti dengan tanggal akhir
+}
+
+# Kirim permintaan GET dengan parameter query
+# response = requests.get("http://okay-hippopotamus-telkomuniversity231-317e9616.koyeb.app/menu_favorites", params=params)
+response = requests.get("http://okay-hippopotamus-telkomuniversity231-317e9616.koyeb.app/sales_performance", params=params)
+
+# Cetak hasil JSON
+print(response.json())
